79ff31e5cc3992d2e0fbe6e7df128696d80ba24f
Chris Haig <chaig200@caledonian.ac.uk>
2019-10-09 17:53:34 +0100

    routing for all gets are now working

--- index.js
+++ index.js
@@ -2,8 +2,7 @@ var express = require("express");
 var app = express();
 var port = 25565;
 
-var routes = require("./routes.js")(app);
-
+require("./routes.js")(app);
 app.listen(port, () => {
  console.log("Server running on port " + port);
 });
--- routes.js
+++ routes.js
@@ -6,17 +6,17 @@ module.exports = function(app){
 
     for (var i = 0, len = headers.length; i < len; i++) {
         const header = headers[i];
-        app.get("/Flight/" + headers[i], (req, res) => {
+        app.get("/Flight/" + header + "/:query?", (req, res) => {
 
-            fs.createReadStream('./flighdata.csv').pipe(csv()).on('data', (row) => {
-                console.log(header);
-              // console.log(row.headers[i])
-                
-    
-            }).on('end', () => {
-                console.log('CSV file successfully processed');
-            });
-             res.json(["Tony","Lisa","Michael","Ginger","Food"]);
+            if(req.params.query != null){
+
+                getRecord(header,req.params.query,res);
+
+            }else{
+                res.send(`<h3>Please follow the following format</h3> <h3>/Flight/${header}/data</h3>`)
+            }
+
+
         });
     }
 
@@ -24,7 +24,7 @@ module.exports = function(app){
 
 
       app.get("/Flight/", (req, res) => {
-       var resp = '<h2>Available Routes</h2>';
+       var resp = `<h2>Available Routes</h2><a href="/Flight/all/"> Get All Records</a><br>`;
         for (var i = 0, len = headers.length; i < len; i++) {
             resp = resp + `<a href="/Flight/${headers[i]}"> Get via ${headers[i]}</a><br>`;
            
@@ -32,4 +32,25 @@ module.exports = function(app){
         res.send(resp)
     });
 
+}
+
+/**
+ * This function filters the document and finds all relevant data to the query stated.
+ * @param {*} header -  The type of information that you are looking for
+ * @param {*} data  -  The query that will filter the data.
+ * @param {*} res  - The response that allows the returning of the objects that have been found
+ */
+function getRecord(header,data,res){
+    var records = []
+    fs.createReadStream('./flighdata.csv').pipe(csv()).on('data', (row) => {
+
+        //If the data matches it will add it to the record array.
+        if(row[header] == data){
+            records.push(row);
+        }
+
+    }).on('end', () => {
+
+        res.send(JSON.stringify(records, null, 3));
+    });
 }
